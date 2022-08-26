eb94fe4e106be554e667571818faf36e9d6bea63
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2022-08-26 15:25:21 -0400

    Update Rudderstack write key

--- frontend/src/app/rudder.ts
+++ frontend/src/app/rudder.ts
@@ -3,7 +3,7 @@ import { isProd } from "src/utils/env";
 
 // only load rudderstack for production
 if (isProd()) {
-  rudderanalytics.load('2Du3FeHmsuj3dJPnDiPyi40UaN1', 'https://fabranickbele.dataplane.rudderstack.com');
+  rudderanalytics.load('2DuH7iesuV4TtpwMqRvXqQttOvm', 'https://fabranickbele.dataplane.rudderstack.com');
 }
 
 export { rudderanalytics };
