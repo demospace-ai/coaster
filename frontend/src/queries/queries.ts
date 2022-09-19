e1effb2a18e8fcc914d203d8423572e69d4144ae
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2022-09-19 15:46:51 -0700

    Keep steps in order and allow repeat steps
    Also use template strings to make queries more readable.

--- frontend/src/queries/queries.ts
+++ frontend/src/queries/queries.ts
@@ -28,24 +28,23 @@ export const getEvents = async (connectionID: number, eventSet: EventSet): Promi
   }
 };
 
-const createStepQuery = (eventSet: EventSet, event: string, previous?: string): string => {
-  const queryArray = [
-    event, "AS", "(",
-    "SELECT DISTINCT", event + "." + eventSet.user_identifier_column + ",", event + "." + eventSet.timestamp_column + ",", "FROM",
-    eventSet.dataset_name + "." + eventSet.table_name, 'AS', event,
-    ...previous ? ["JOIN", previous, "ON", previous + "." + eventSet.user_identifier_column, "=", event + "." + eventSet.user_identifier_column] : [],
-    "WHERE", event + '.' + eventSet.event_type_column, "=", "'" + event + "'",
-    ...previous ? ["AND", previous + "." + eventSet.timestamp_column, "<", event + "." + eventSet.timestamp_column] : [],
-    ")"
-  ];
-  return queryArray.join(' ');
+const createStepSubquery = (eventSet: EventSet, event: string, order: number, previous?: string): string => {
+  const tableName = `${event}_${order}`;
+  const queryArray = `
+    ${!previous ? "WITH " : ""}${tableName} AS (
+      SELECT DISTINCT ${tableName}.${eventSet.user_identifier_column}, ${tableName}.${eventSet.timestamp_column}
+      FROM ${eventSet.dataset_name}.${eventSet.table_name} AS ${tableName}
+      ${previous ? `JOIN ${previous} ON ${previous}.${eventSet.user_identifier_column} = ${tableName}.${eventSet.user_identifier_column}` : ``}
+      WHERE ${tableName}.${eventSet.event_type_column} = '${event}'
+      ${previous ? `AND ${previous}.${eventSet.timestamp_column} < ${tableName}.${eventSet.timestamp_column}` : ``}
+    )
+  `;
+  return queryArray;
 };
 
-const createResultsQuery = (eventSet: EventSet, events: string[]): string => {
-  const rollupArray = events.map(event => {
-    return [
-      "SELECT COUNT(DISTINCT", eventSet.user_identifier_column, ") AS count,", "'" + event + "'", "AS event FROM", event,
-    ].join(' ');
+const createResultsSubquery = (eventSet: EventSet, events: string[]): string => {
+  const rollupArray = events.map((event, index) => {
+    return `SELECT COUNT(DISTINCT ${eventSet.user_identifier_column}) AS count, '${event}' AS event, ${index} AS event_order FROM ${event}_${index}`;
   });
 
   const queryArray = [
@@ -58,18 +57,16 @@ const createResultsQuery = (eventSet: EventSet, events: string[]): string => {
 };
 
 export const runFunnelQuery = async (connectionID: number, eventSet: EventSet, events: string[]): Promise<RunQueryResponse> => {
-  const stepQueryArray = events.map((event, index) => {
-    return createStepQuery(eventSet, event, index === 0 ? undefined : events[index - 1]);
+  const stepSubqueryArray = events.map((event, index) => {
+    return createStepSubquery(eventSet, event, index, index === 0 ? undefined : `${events[index - 1]}_${index - 1}`);
   });
-  const stepQueryString = stepQueryArray.join(", ");
+  const stepSubqueryString = stepSubqueryArray.join(", ");
 
-  const queryArray = [
-    "WITH", stepQueryString, ",",
-    createResultsQuery(eventSet, events),
-    "SELECT count, event, (count / (SELECT MAX(count) FROM results)) as percent from results"
-  ];
-
-  const query = queryArray.join(' ');
+  const query = `
+    ${stepSubqueryString},
+    ${createResultsSubquery(eventSet, events)}
+    SELECT count, event, (count / (SELECT MAX(count) FROM results)) as percent from results ORDER BY results.event_order
+  `;
 
   const payload: RunQueryRequest = {
     'connection_id': connectionID,
