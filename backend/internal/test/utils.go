f017d345e4d00e5216d3dc3f2774910895c22d6a
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2022-09-27 23:19:23 -0700

    Try again with container IP

--- backend/internal/test/utils.go
+++ backend/internal/test/utils.go
@@ -44,7 +44,7 @@ func SetupDatabase() (*gorm.DB, func()) {
 		log.Fatalf("Could not start resource: %s", err)
 	}
 
-	host := "127.0.0.1"
+	host := resource.Container.NetworkSettings.Networks["cloudbuild"].IPAddress
 	port := resource.GetPort("5432/tcp")
 	dbURI := fmt.Sprintf("user=fabratest password=fabratest database=fabratest host=%s port=%s", host, port)
 
