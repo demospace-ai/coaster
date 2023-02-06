99862e314586db44060f2cd7d338e494d6a5a5f9
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-02-06 14:11:15 -0800

    More updates to reflect universal API use case

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,10 @@
 import React, { ReactNode, useEffect } from 'react';
-import { Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { useStart } from 'src/app/actions';
 import { Header } from 'src/components/header/Header';
 import { Loading } from 'src/components/loading/Loading';
 import { NavigationBar } from 'src/components/navigationBar/NavigationBar';
-import { Dashboard } from 'src/pages/dashboards/Dashboard';
-import { Dashboards } from 'src/pages/dashboards/Dashboards';
 import { Inbox } from 'src/pages/inbox/Inbox';
-import { CustomQuery } from 'src/pages/insights/customquery/CustomQuery';
-import { Funnel } from 'src/pages/insights/funnel/Funnel';
-import { Insights } from 'src/pages/insights/Insights';
-import { Trend } from 'src/pages/insights/trend/Trend';
 import { Login } from 'src/pages/login/Login';
 import { NotFound } from 'src/pages/notfound/NotFound';
 import { WorkspaceSettings } from 'src/pages/workspacesettings/WorkspaceSettings';
@@ -22,7 +16,6 @@ let needsInit = true;
 export const App: React.FC = () => {
   const loading = useSelector(state => state.app.loading);
   const start = useStart();
-  let location = useLocation();
 
   useEffect(() => {
     // Recommended way to run one-time initialization: https://beta.reactjs.org/learn/you-might-not-need-an-effect#initializing-the-application
@@ -44,13 +37,7 @@ export const App: React.FC = () => {
         <Route element={<AppLayout />}>
           <Route path='/login' element={<Login />} />
           <Route path='/' element={<RequireAuth element={<Inbox />} />} />
-          <Route path='/workspacesettings' element={<RequireAuth element={<WorkspaceSettings />} />} />
-          <Route path='/customquery/:id' element={<RequireAuth element={<CustomQuery key={location.pathname} />} />} />
-          <Route path='/funnel/:id' element={<RequireAuth element={<Funnel key={location.pathname} />} />} />
-          <Route path='/trend/:id' element={<RequireAuth element={<Trend key={location.pathname} />} />} />
-          <Route path='/insights' element={<RequireAuth element={<Insights />} />} />
-          <Route path='/dashboard/:id' element={<RequireAuth element={<Dashboard key={location.pathname} />} />} />
-          <Route path='/dashboards' element={<RequireAuth element={<Dashboards />} />} />
+          <Route path='/settings' element={<RequireAuth element={<WorkspaceSettings />} />} />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
--- frontend/src/components/navigationBar/NavigationBar.tsx
+++ frontend/src/components/navigationBar/NavigationBar.tsx
@@ -1,7 +1,8 @@
-import { ChartBarSquareIcon, Cog6ToothIcon, HomeIcon } from '@heroicons/react/24/outline';
+import { Cog6ToothIcon, HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline';
 import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 import { DashboardIcon, QuestionCircleIcon } from "src/components/icons/Icons";
+import { Tooltip } from 'src/components/tooltip/Tooltip';
 import { useSelector } from "src/root/model";
 import logo from './logo.png';
 
@@ -14,19 +15,22 @@ export const NavigationBar: React.FC = () => {
     return <></>;
   };
 
-  const route = "tw-inline-block tw-pt-[1px] tw-ml-[8px]";
-  const routeContainer = "tw-relative tw-flex tw-flex-row tw-h-8 tw-box-border tw-cursor-pointer tw-items-center tw-text-gray-800 tw-mt-0 tw-mb-[3px] tw-mx-2 tw-rounded-md tw-select-none";
+  const route = "tw-inline-block tw-pt-[0.5px] tw-ml-2.5 tw-font-medium";
+  const routeContainer = "tw-relative tw-flex tw-flex-row tw-h-9 tw-box-border tw-cursor-pointer tw-items-center tw-text-gray-800 tw-mt-0.5 tw-mb-0.5 tw-mx-2 tw-rounded-md tw-select-none";
   const navLink = "tw-w-full tw-h-full tw-pl-3 tw-rounded-md tw-flex tw-flex-row tw-items-center hover:tw-bg-gray-300";
 
   return (
     <>
       <div className="tw-min-w-[240px] tw-w-60 tw-h-full tw-flex tw-flex-col tw-box-border tw-border-r tw-border-solid tw-border-gray-200 tw-bg-gray-100">
-        <NavLink className="tw-py-4 tw-px-4 tw-flex tw-flex-row tw-h-16 tw-box-border tw-border-b tw-border-solid tw-border-gray-200 tw-cursor-pointer tw-w-full tw-mb-4" to="/">
+        <NavLink className="tw-py-4 tw-px-4 tw-flex tw-flex-row tw-h-16 tw-box-border tw-cursor-pointer tw-w-full tw-mb-4" to="/">
           <img src={logo} className='tw-h-6 tw-w-6 tw-justify-center tw-items-center tw-rounded tw-flex tw-my-auto tw-select-none' alt="fabra logo" />
-          <div className='tw-my-auto tw-ml-2.5 tw-text-base tw-text-ellipsis tw-max-w-[150px] tw-whitespace-nowrap tw-overflow-hidden tw-select-none tw-font-bold'>
-            {organization!.name}
+          <div className='tw-my-auto tw-ml-3 tw-max-w-[150px] tw-whitespace-nowrap tw-overflow-hidden tw-select-none tw-font-bold tw-font-[Montserrat] tw-text-2xl'>
+            fabra
           </div>
         </NavLink>
+        <div className="tw-mx-4 tw-mb-2 tw-uppercase tw-text-xs tw-text-gray-500 tw-font-medium">
+          Overview
+        </div>
         <div className={routeContainer}>
           <NavLink className={({ isActive }) => classNames(navLink, isActive && "tw-bg-gray-300")} to={'/'}>
             <HomeIcon className="tw-h-4" strokeWidth="2" />
@@ -34,20 +38,24 @@ export const NavigationBar: React.FC = () => {
           </NavLink>
         </div>
         <div className={routeContainer}>
-          <NavLink className={({ isActive }) => classNames(navLink, isActive && "tw-bg-gray-300")} to={'/insights'}>
-            <ChartBarSquareIcon className="tw-h-4" strokeWidth="2" />
-            <div className={route}>Insights</div>
-          </NavLink>
+          <Tooltip label="Coming soon!">
+            <div className={navLink}>
+              <UserGroupIcon className="tw-h-4" strokeWidth="2" />
+              <div className={route}>Linked Accounts</div>
+            </div>
+          </Tooltip>
         </div>
         <div className={routeContainer}>
-          <NavLink className={({ isActive }) => classNames(navLink, isActive && "tw-bg-gray-300")} to={'/help'}>
-            <DashboardIcon className="tw-h-4" strokeWidth="2" />
-            <div className={route}>Logs</div>
-          </NavLink>
+          <Tooltip label="Coming soon!">
+            <div className={navLink}>
+              <DashboardIcon className="tw-h-4" strokeWidth="2" />
+              <div className={route}>Logs</div>
+            </div>
+          </Tooltip>
         </div>
         <div id="bottomSection" className="tw-mt-auto tw-mb-5">
           <div className={routeContainer}>
-            <NavLink className={navLink} to="/workspacesettings">
+            <NavLink className={({ isActive }) => classNames(navLink, isActive && "tw-bg-gray-300")} to="/settings">
               <Cog6ToothIcon className="tw-h-4 tw-ml-[1px] -tw-mr-[0.5px]" strokeWidth="2" />
               <div className={route}>Settings</div>
             </NavLink>
