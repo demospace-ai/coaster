d6444c6222bf10f9d40aa8571f5b924549b7a776
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-02-23 00:22:03 -0800

    Forgot to add new files

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+  }
+});
