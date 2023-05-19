03d3adc820c67dfdcfeeb2f75f0f0b02206eab6d
Alan Gou <alanjgou@gmail.com>
2023-05-19 16:47:50 -0400

    Update the button

--- frontend/src/pages/syncs/Sync.tsx
+++ frontend/src/pages/syncs/Sync.tsx
@@ -74,6 +74,7 @@ export const Sync: React.FC = () => {
               className="tw-ml-auto tw-px-8 tw-py-2 tw-rounded-md tw-font-medium tw-text-base tw-bg-blue-600 hover:tw-bg-blue-500 tw-text-white tw-mr-2 tw-relative disabled:tw-bg-gray-500"
               onClick={handleRunSync}
             >
+
               <div className="tw-absolute tw-left-2 tw-top-1/2 tw-transform -tw-translate-y-1/2">
                 {renderButtonStatus()}
               </div>
