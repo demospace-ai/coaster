3e3617541b72ac6fdc67486fa8fd05d9964e4b71
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-05-22 18:56:22 -0700

    go-rwx

--- backend/server/dev/generateCerts.sh
+++ backend/server/dev/generateCerts.sh
@@ -4,5 +4,5 @@ mkdir -p certs
 
 # Generate server certs (provided to Postgres instance)
 openssl req -new -x509 -days 365 -nodes -text -out certs/server.crt -keyout certs/server.key -subj "/CN=db"
-chmod 400 certs/server.key
-chmod 400 certs/server.crt
+chmod go-rwx certs/server.key
+chmod go-rwx certs/server.crt
