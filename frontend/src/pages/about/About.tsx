b6d2b96a8b75de2c4423e745968808b5145d26eb
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-08-15 16:02:57 -0700

    Add About page

---
+++ frontend/src/pages/about/About.tsx
@@ -0,0 +1,14 @@
+export const About: React.FC = () => {
+  return (
+    <div className="tw-flex tw-h-full tw-w-full tw-items-center tw-justify-center">
+      <div className="tw-max-w-[600px] tw-mb-64 tw-mt-10">
+        <div className="tw-text-5xl tw-font-bold tw-mb-8">Find your next adventure</div>
+        <p className="tw-text-center tw-text-base">
+          Our marketplace is curated to help you find your dream adventure. Discover amazing trips, book with our
+          money-back guarantee, and coordinate easily with your guide. Coaster is bringing adventure travel to a new
+          level.
+        </p>
+      </div>
+    </div>
+  );
+};
