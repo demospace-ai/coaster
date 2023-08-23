51879ffd51707492f791a8038bdfdcf5ed38547b
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-08-22 19:00:14 -0700

    Fix duration rounding

--- frontend/src/pages/listing/Listing.tsx
+++ frontend/src/pages/listing/Listing.tsx
@@ -253,15 +253,15 @@ const getMaxGuests = (listing: ListingType) => {
 
 const getDuration = (listing: ListingType) => {
   if (listing.duration_minutes) {
-    const days = listing.duration_minutes / 1440; // 1440 minutes in a day
-    if (days > 0) {
-      const roundedDays = Math.round(days);
+    const daysFloor = Math.floor(listing.duration_minutes / 1440); // 1440 minutes in a day
+    if (daysFloor > 0) {
+      const roundedDays = Math.round(listing.duration_minutes / 1440);
       return `${roundedDays} day${roundedDays > 1 ? "s" : ""}`;
     }
 
-    const hours = listing.duration_minutes / 60;
-    if (hours > 0) {
-      const roundedHours = Math.round(hours);
+    const hoursFloor = Math.floor(listing.duration_minutes / 60);
+    if (hoursFloor > 0) {
+      const roundedHours = Math.round(listing.duration_minutes / 60);
       return `${roundedHours} hour${roundedHours > 1 ? "s" : ""}`;
     }
 
