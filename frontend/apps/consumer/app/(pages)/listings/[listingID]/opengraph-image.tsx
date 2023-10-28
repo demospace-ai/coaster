584eb82c8322da54d8e7fe229abe7c48ca370fa1
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-10-28 19:02:32 -0400

    New opengraph image

--- frontend/apps/consumer/app/(pages)/listings/[listingID]/opengraph-image.tsx
+++ frontend/apps/consumer/app/(pages)/listings/[listingID]/opengraph-image.tsx
@@ -1,9 +1,9 @@
+import { getListingServer } from "@coaster/rpc/server";
+import { getGcsImageUrl } from "@coaster/utils/common";
 import { ImageResponse } from "next/server";
 
-// Route segment config
 export const runtime = "edge";
 
-// Image metadata
 export const alt = "Coaster - Find your next adventure";
 export const size = {
   width: 1200,
@@ -12,30 +12,20 @@ export const size = {
 
 export const contentType = "image/png";
 
-// Image generation
-export default async function Image() {
-  // Font
+export default async function Image({ params }: { params: { listingID: string } }) {
+  const listing = await getListingServer(Number(params.listingID));
+  if (!listing || listing.images.length === 0) {
+    return undefined;
+  }
+
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        Coaster - Find your next adventure
-      </div>
+      <img
+        src={getGcsImageUrl(listing.images[0].storage_id)}
+        style={{ width: size.width, height: size.height, objectFit: "cover" }}
+      ></img>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
       ...size,
     },
   );
