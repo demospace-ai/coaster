7b94e0e157eeb65c52f252f5b04c1e1b224633ce
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-10-28 19:24:59 -0400

    Add hero blur image

--- frontend/apps/consumer/app/(pages)/page.tsx
+++ frontend/apps/consumer/app/(pages)/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@coaster/assets";
+import { Hero, HeroBlur } from "@coaster/assets";
 import { SearchBar } from "@coaster/components/client";
 import { getFeaturedServer } from "@coaster/rpc/server";
 import { Featured } from "consumer/app/(pages)/client";
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export const dynamic = "force-static";
 
-export const viewport = {
+export const metadata = {
   themeColor: "#efedea",
 };
 
@@ -18,7 +18,15 @@ export default async function Page() {
       <div className="tw-flex tw-flex-col tw-items-center tw-pt-2 tw-pb-24 tw-w-full tw-max-w-[1280px]">
         <div className="tw-relative tw-flex tw-flex-col tw-mb-6 sm:tw-mb-10 tw-mx-10 tw-w-full tw-h-[480px] tw-rounded-2xl tw-items-center tw-justify-center tw-bg-cover tw-p-8">
           <div className="tw-absolute tw-top-0 tw-left-0 tw-h-full tw-w-full">
-            <Image fill priority src={Hero.src} alt="" className="tw-rounded-2xl tw-object-left tw-object-cover" />
+            <Image
+              fill
+              priority
+              src={Hero.src}
+              blurDataURL={HeroBlur.src}
+              placeholder="blur"
+              alt=""
+              className="tw-rounded-2xl tw-object-left tw-object-cover"
+            />
           </div>
           <div className="tw-z-[1]">
             <div className="tw-text-white tw-w-full tw-max-w-[800px] tw-py-5 tw-rounded-2xl tw-text-center">
