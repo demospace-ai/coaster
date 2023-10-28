4f1fbde73bcd9660466f5deaa429b3a5ee110622
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-10-28 19:48:00 -0400

    Optimize about image

--- frontend/packages/components/src/pages/About.tsx
+++ frontend/packages/components/src/pages/About.tsx
@@ -1,13 +1,16 @@
 import { Founders } from "@coaster/assets";
+import Image from "next/image";
 
 export const About: React.FC = () => {
   return (
     <div className="tw-flex tw-items-center tw-justify-center tw-py-24 sm:tw-py-32">
       <div className="tw-flex tw-flex-col tw-items-center tw-max-w-[300px] sm:tw-max-w-[600px]">
-        <img
-          className="tw-rounded-xl tw-mb-5 sm:tw-mb-8 tw-max-w-[80%] sm:tw-max-w-xs"
+        <Image
           src={Founders.src}
           alt="The founders of Coaster"
+          width={320}
+          height={320}
+          className="tw-rounded-xl tw-mb-5 sm:tw-mb-8 sm:tw-max-w-xs"
         />
         <div className="tw-text-center tw-text-4xl sm:tw-text-5xl tw-font-bold tw-mb-4 sm:tw-mb-8">
           Find your next adventure
