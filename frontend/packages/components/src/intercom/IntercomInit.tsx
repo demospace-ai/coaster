8c69250124b788683d87106343acb0dabaefb55a
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-01 13:50:25 -0400

    Use lazy load strategy for Intercom script

--- frontend/packages/components/src/intercom/IntercomInit.tsx
+++ frontend/packages/components/src/intercom/IntercomInit.tsx
@@ -40,7 +40,7 @@ export const IntercomInit = () => {
           app_id: "pdc06iv8",
         };`}
       </Script>
-      <Script id="intercom-init">
+      <Script id="intercom-init" strategy="lazyOnload">
         {
           // eslint-disable-next-line @typescript-eslint/quotes
           `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/pdc06iv8';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`
