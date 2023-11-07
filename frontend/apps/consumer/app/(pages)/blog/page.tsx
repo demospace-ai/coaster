257c34452525593da1bc29cdb896f8abbc1820d7
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-07 11:08:35 -0500

    Fix blog list styling

--- frontend/apps/consumer/app/(pages)/blog/page.tsx
+++ frontend/apps/consumer/app/(pages)/blog/page.tsx
@@ -16,6 +16,7 @@ export default async function AllPosts() {
         title,
         slug,
         metaDescription,
+        "authorName": author->name,
         mainImage{
           ...,
           "metadata": asset->metadata,
@@ -27,11 +28,11 @@ export default async function AllPosts() {
   return (
     <main className="tw-w-full tw-max-w-7xl tw-mt-10 tw-mx-4 sm:tw-mx-10">
       <div className="tw-mb-10">
-        <h1 className="tw-font-bold tw-text-6xl">Blog</h1>
+        <h1 className="tw-font-bold tw-text-5xl">Blog</h1>
       </div>
-      <div className="tw-flex tw-w-full tw-justify-start">
+      <div className="tw-grid tw-grid-flow-row-dense tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 tw-gap-10 tw-w-full tw-justify-start">
         {posts.map((post) => (
-          <Link href={`/blog/${post.slug.current}`} key={post._id} className="tw-flex tw-flex-col tw-max-w-2xl">
+          <Link href={`/blog/${post.slug.current}`} key={post._id} className="tw-flex tw-flex-col tw-max-w-xl">
             <Image
               priority
               alt={post.mainImage.alt}
@@ -43,7 +44,8 @@ export default async function AllPosts() {
               blurDataURL={post.mainImage.metadata.lqip}
               className="tw-shadow-md"
             />
-            <div className="tw-mt-4 tw-font-bold tw-text-2xl">{post.title}</div>
+            <div className="tw-mt-4 tw-uppercase">By {post.authorName}</div>
+            <div className="tw-mt-1 tw-font-bold tw-text-2xl">{post.title}</div>
             <div className="tw-mt-1 tw-text-base">{post.metaDescription}</div>
           </Link>
         ))}
