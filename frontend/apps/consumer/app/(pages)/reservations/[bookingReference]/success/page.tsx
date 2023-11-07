b26392a66fcc8a9d99e982305c53bcf327c6dad8
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-07 15:16:57 -0500

    Remove unused booking success page

--- frontend/apps/consumer/app/(pages)/reservations/[bookingReference]/success/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Success({ params }: { params: { bookingID: string } }) {
-  return (
-    <main className="tw-flex tw-w-full tw-justify-center tw-px-5 sm:tw-px-20">
-      <div className="tw-flex tw-flex-col tw-items-center tw-pt-5 sm:tw-pt-8 tw-pb-24 tw-w-full tw-max-w-[1280px]">
-        <div className="tw-font-bold tw-text-xl tw-w-full tw-text-center sm:tw-text-left">hi</div>
-      </div>
-    </main>
-  );
-}
