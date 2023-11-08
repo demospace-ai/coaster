a6cac17e5b181bb6d8275ee81adf0a77edd07064
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-08 15:47:49 -0500

    Add spacing between reservations

--- frontend/apps/consumer/app/(pages)/reservations/page.tsx
+++ frontend/apps/consumer/app/(pages)/reservations/page.tsx
@@ -28,29 +28,31 @@ export default async function Reservations() {
             </Link>
           </>
         ) : (
-          bookings.map((booking) => (
-            <Link key={booking.reference} className="tw-flex tw-text-lg" href={`/reservations/${booking.reference}`}>
-              <Image
-                src={booking.booking_image.url}
-                width={booking.booking_image.width}
-                height={booking.booking_image.height}
-                alt="Booking image"
-                sizes="(max-width: 639px) 128px, 192px"
-                placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
-                tabIndex={-1}
-                className="tw-w-20 sm:tw-w-32"
-              />
-              <div className="tw-flex tw-flex-col tw-ml-5">
-                <div className="tw-font-bold">
-                  {booking.reference} - {booking.listing.name}
+          <div className="tw-flex tw-flex-col tw-gap-4">
+            {bookings.map((booking) => (
+              <Link key={booking.reference} className="tw-flex tw-text-lg" href={`/reservations/${booking.reference}`}>
+                <Image
+                  src={booking.booking_image.url}
+                  width={booking.booking_image.width}
+                  height={booking.booking_image.height}
+                  alt="Booking image"
+                  sizes="(max-width: 639px) 128px, 192px"
+                  placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
+                  tabIndex={-1}
+                  className="tw-w-20 sm:tw-w-32"
+                />
+                <div className="tw-flex tw-flex-col tw-ml-5">
+                  <div className="tw-font-bold">
+                    {booking.reference} - {booking.listing.name}
+                  </div>
+                  <div>{booking.listing.location}</div>
+                  <div>
+                    {getStartTimeString(booking.start_date, booking.start_time, booking.listing.availability_type)}
+                  </div>
                 </div>
-                <div>{booking.listing.location}</div>
-                <div>
-                  {getStartTimeString(booking.start_date, booking.start_time, booking.listing.availability_type)}
-                </div>
-              </div>
-            </Link>
-          ))
+              </Link>
+            ))}
+          </div>
         )}
         <div className="tw-text-sm tw-w-full tw-text-center sm:tw-text-left tw-pt-6 tw-mt-16 tw-border-t tw-border-solid tw-border-gray-300">
           Can't find your reservation? <HelpButton />
