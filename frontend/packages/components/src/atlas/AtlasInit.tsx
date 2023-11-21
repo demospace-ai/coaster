7182b193d122b9eb7eac61d618b32d8d97787f65
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-21 16:39:58 -0500

    New approach for loading Atlas

--- frontend/packages/components/src/atlas/AtlasInit.tsx
+++ frontend/packages/components/src/atlas/AtlasInit.tsx
@@ -2,17 +2,36 @@
 
 import { isProd } from "@coaster/utils/common";
 import Script from "next/script";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    Atlas:
+      | {
+          appId: string;
+          v: number;
+          q: unknown[];
+          call: (...args: unknown[]) => void;
+        }
+      | undefined;
+  }
+}
 
 export const AtlasInit = () => {
   if (!isProd()) return null;
 
-  return (
-    <>
-      <Script id="intercom-settings">
-        {`(()=>{"use strict";var t,e={appId:"vn78sbkf0q",v:2,q:[],call:function(){this.q.push(arguments)}};
-        window.Atlas=e;var n=document.createElement("script");n.async=!0,n.src="https://app.atlas.so/client-js/atlas.bundle.js";
-        var s=document.getElementsByTagName("script")[0];null===(t=s.parentNode)||void 0===t||t.insertBefore(n,s)})();window.Atlas.start()`}
-      </Script>
-    </>
-  );
+  useEffect(function atlasSnippetEntry() {
+    if (typeof window.Atlas === "object") return;
+
+    window.Atlas = {
+      appId: "vn78sbkf0q",
+      v: 2,
+      q: [["start"]],
+      call: function () {
+        this.q?.push(arguments);
+      },
+    };
+  }, []);
+
+  return <Script id="atlas-bundle" src="https://app.atlas.so/client-js/atlas.bundle.js" strategy="lazyOnload" />;
 };
