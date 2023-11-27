10655cee743bef27b4035a15071cf7e1aa65307d
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-27 15:47:37 -0500

    Fix categories again

--- frontend/apps/consumer/app/(pages)/[category]/utils.tsx
+++ frontend/apps/consumer/app/(pages)/[category]/utils.tsx
@@ -1,4 +1,4 @@
 import { Category } from "@coaster/types";
 
-const lits = [...Object.keys(Category), "daytrips", undefined];
-export type FeaturedCategory = (typeof lits)[number];
+const categories = [...Object.keys(Category), "daytrips", undefined];
+export type FeaturedCategory = (typeof categories)[number];
