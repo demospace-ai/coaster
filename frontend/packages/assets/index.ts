675ff7df32adea392db30a24e178335f46c77274
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-28 18:05:18 -0500

    Update hero image

--- frontend/packages/assets/index.ts
+++ frontend/packages/assets/index.ts
@@ -1,7 +1,6 @@
 export { default as Coordinates } from "./src/coordinates.svg";
 export { default as Founders } from "./src/founders.webp";
-export { default as HeroBlur } from "./src/hero-blur.webp";
-export { default as Hero } from "./src/hero.webp";
+export { default as Hero } from "./src/hero.jpg";
 export { default as Logo } from "./src/logo.svg";
 export { default as LongLogo } from "./src/long-logo.svg";
 export { default as Mail } from "./src/mail.svg";
