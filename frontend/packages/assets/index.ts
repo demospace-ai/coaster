f24779c9a517231feb9081b33425445c75fe80a6
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-11-28 18:55:34 -0500

    Update size of mobile hero

--- frontend/packages/assets/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Coordinates } from "./src/coordinates.svg";
-export { default as Founders } from "./src/founders.webp";
-export { default as Hero } from "./src/hero.jpg";
-export { default as Logo } from "./src/logo.svg";
-export { default as LongLogo } from "./src/long-logo.svg";
-export { default as Mail } from "./src/mail.svg";
-export { default as MapPreview } from "./src/map-preview.webp";
