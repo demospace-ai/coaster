c72d5e35ba2174bee10aa749c977c021bc33cea5
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-04 16:24:04 -0500

    Fix revalidation endpoint

--- frontend/apps/consumer/app/api/revalidate/listings/[listingID]/route.ts
+++ frontend/apps/consumer/app/api/revalidate/listings/[listingID]/route.ts
@@ -1,6 +1,7 @@
 import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
-export async function POST({ params }: { params: { listingID: string } }) {
+export async function POST(_: NextRequest, { params }: { params: { listingID: string } }) {
   const listingID = params.listingID;
   revalidatePath(`/listings/${listingID}`);
 }
