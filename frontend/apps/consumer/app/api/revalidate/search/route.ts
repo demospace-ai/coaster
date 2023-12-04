8384a58cbcbf005419e2aa65707759187ba5df58
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-04 17:33:43 -0500

    Update revalidation endpoint for search

--- frontend/apps/consumer/app/api/revalidate/search/route.ts
+++ frontend/apps/consumer/app/api/revalidate/search/route.ts
@@ -3,6 +3,6 @@ import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams.toString();
-  revalidatePath(`/search?${params}`);
+  revalidatePath(`/listings?${params}`);
   return Response.json({ revalidated: true, now: Date.now() });
 }
