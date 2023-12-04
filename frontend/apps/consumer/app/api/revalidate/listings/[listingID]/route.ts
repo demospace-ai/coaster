43dfe051e02e083d3b26278a9e833bc37eeb4caf
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-04 16:29:19 -0500

    Return response from revalidate endpoints

--- frontend/apps/consumer/app/api/revalidate/listings/[listingID]/route.ts
+++ frontend/apps/consumer/app/api/revalidate/listings/[listingID]/route.ts
@@ -4,4 +4,5 @@ import { NextRequest } from "next/server";
 export async function POST(_: NextRequest, { params }: { params: { listingID: string } }) {
   const listingID = params.listingID;
   revalidatePath(`/listings/${listingID}`);
+  return Response.json({ revalidated: true, now: Date.now() });
 }
--- frontend/apps/consumer/app/api/revalidate/search/route.ts
+++ frontend/apps/consumer/app/api/revalidate/search/route.ts
@@ -4,4 +4,5 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams.toString();
   revalidatePath(`/search?${params}`);
+  return Response.json({ revalidated: true, now: Date.now() });
 }
