bc0873e4ef493ae45d233b106db2ca3b51c059f2
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-05 14:16:57 -0500

    Return update revalidate params

--- frontend/apps/consumer/app/api/revalidate/search/route.ts
+++ frontend/apps/consumer/app/api/revalidate/search/route.ts
@@ -4,5 +4,5 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams.toString();
   revalidatePath(`/listings?${params}`);
-  return Response.json({ revalidated: true, now: Date.now() });
+  return Response.json({ revalidated: true, now: Date.now(), params: params });
 }
