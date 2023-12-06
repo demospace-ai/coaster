7e8997a6287e52efeaec039aac6871263fdc37ea
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-06 12:24:14 -0500

    Fix duration display

--- frontend/packages/utils/src/listing.ts
+++ frontend/packages/utils/src/listing.ts
@@ -2,13 +2,13 @@ export function getDuration(durationMinutes: number | undefined): string {
   if (durationMinutes) {
     const daysFloor = Math.floor(durationMinutes / 1440); // 1440 minutes in a day
     if (daysFloor > 0) {
-      const roundedDays = Math.round(durationMinutes / 1440);
+      const roundedDays = Math.ceil(durationMinutes / 1440);
       return `${roundedDays} day${roundedDays > 1 ? "s" : ""}`;
     }
 
     const hoursFloor = Math.floor(durationMinutes / 60);
     if (hoursFloor > 0) {
-      const roundedHours = Math.round(durationMinutes / 60);
+      const roundedHours = Math.ceil(durationMinutes / 60);
       return `${roundedHours} hour${roundedHours > 1 ? "s" : ""}`;
     }
 
