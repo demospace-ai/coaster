087af7c1af6bec7743f00754b408343c0967bb1d
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-06 17:14:22 -0500

    Remove logging

--- frontend/apps/consumer/app/api/revalidate/search/route.ts
+++ frontend/apps/consumer/app/api/revalidate/search/route.ts
@@ -4,8 +4,6 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams.toString();
   const decodedParams = decodeURI(params);
-  console.log(params);
-  console.log(decodedParams);
   revalidatePath(`/listings?${decodedParams}`);
   return Response.json({ revalidated: true, now: Date.now(), params: decodedParams });
 }
