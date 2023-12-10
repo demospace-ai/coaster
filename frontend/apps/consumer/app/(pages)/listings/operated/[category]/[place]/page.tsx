c5e595c88d21dfacfa35980cd988296b56799bd7
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-10 16:00:41 -0500

    Update about message

--- frontend/apps/consumer/app/(pages)/listings/operated/[category]/[place]/page.tsx
+++ frontend/apps/consumer/app/(pages)/listings/operated/[category]/[place]/page.tsx
@@ -46,7 +46,7 @@ export default async function GeneratedListing({
       last_name: "",
       profile_picture_url: "https://www.trycoaster.com/icon.png",
       about:
-        "Coaster is on a mission to make outdoor adventures more accessible. We're accomplishing this goal by delivering top-tier programs that adhere to the highest safety and quality standards that are fully planned and led by experienced guides. Through these offerings, we carve out a space for individuals to break away from the fast-paced routines of daily life, without needing to spend hours planning and researching. In doing so, we hope to facilitate people's reconnection with the wonders of our natural world.",
+        "Coaster is on a mission to make outdoor adventures more accessible. We're accomplishing this goal by delivering top-tier programs that adhere to the highest safety and quality standards that are fully planned and led by experienced guides.",
       email: "trips@trycoaster.com",
     },
     images: getImagesForGeneratedListing(params.category),
