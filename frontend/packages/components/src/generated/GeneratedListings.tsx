76612c5592ab3cd0792e8aaf333e4d442c4de5bc
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-13 15:36:44 -0500

    Add hover effect for listing results

--- frontend/packages/components/src/generated/GeneratedListings.tsx
+++ frontend/packages/components/src/generated/GeneratedListings.tsx
@@ -59,16 +59,18 @@ export const GeneratedSearchResult: React.FC<{ listing: GeneratedListing; classN
       target="_blank"
       rel="noreferrer"
     >
-      <Image
-        width={image.width}
-        height={image.height}
-        alt="Listing image"
-        sizes="(max-width: 400px) 75vw, (max-width: 640px) 30vw, (max-width: 768px) 25vw, (max-width: 1024px) 20vw, (max-width: 1536px) 18vw, (max-width: 2000px) 15vw, 12vw"
-        placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
-        tabIndex={-1}
-        className="tw-flex tw-rounded-xl tw-aspect-square tw-flex-none tw-object-cover"
-        src={image.url}
-      />
+      <div className="tw-flex tw-rounded-xl tw-aspect-square tw-overflow-hidden">
+        <Image
+          width={image.width}
+          height={image.height}
+          alt="Listing image"
+          sizes="(max-width: 400px) 75vw, (max-width: 640px) 30vw, (max-width: 768px) 25vw, (max-width: 1024px) 20vw, (max-width: 1536px) 18vw, (max-width: 2000px) 15vw, 12vw"
+          placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
+          tabIndex={-1}
+          className="tw-w-full tw-h-full tw-object-cover hover:tw-scale-105 tw-ease-in-out tw-transition-all tw-duration-200"
+          src={image.url}
+        />
+      </div>
       <span className="tw-mt-2 sm:tw-mt-3 tw-font-bold tw-text-lg">
         {getCategoryForDisplay(listing.category) + " Experience by Coaster"}
       </span>
--- frontend/packages/components/src/search/SearchResult.tsx
+++ frontend/packages/components/src/search/SearchResult.tsx
@@ -24,16 +24,18 @@ export const SearchResult: React.FC<{ listing: Listing; className?: string }> = 
           <HeartIcon className="tw-w-6  hover:tw-w-5 tw-transition-all tw-duration-100 tw-text-gray-600" />
         </div>
       */}
-      <Image
-        width={listing.images[0].width}
-        height={listing.images[0].height}
-        alt="Listing image"
-        sizes="(max-width: 400px) 75vw, (max-width: 640px) 30vw, (max-width: 768px) 25vw, (max-width: 1024px) 20vw, (max-width: 1536px) 18vw, (max-width: 2000px) 15vw, 12vw"
-        placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
-        tabIndex={-1}
-        className="tw-flex tw-rounded-xl tw-aspect-square tw-flex-none tw-object-cover"
-        src={listing.images[0].url}
-      />
+      <div className="tw-flex tw-rounded-xl tw-aspect-square tw-overflow-hidden">
+        <Image
+          width={listing.images[0].width}
+          height={listing.images[0].height}
+          alt="Listing image"
+          sizes="(max-width: 400px) 75vw, (max-width: 640px) 30vw, (max-width: 768px) 25vw, (max-width: 1024px) 20vw, (max-width: 1536px) 18vw, (max-width: 2000px) 15vw, 12vw"
+          placeholder="data:image/svg+xml;base64,PHN2ZwogICAgICB3aWR0aD0iMTAwJSIKICAgICAgaGVpZ2h0PSIxMDAlIgogICAgICB2aWV3Qm94PSIwIDAgMTAwIDEwMCIKICAgICAgdmVyc2lvbj0iMS4xIgogICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciCiAgICAgIHhtbG5zWGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiCiAgICA+CiAgICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9IiNlZWUiPgogICAgICAgIDxhbmltYXRlIGF0dHJpYnV0ZU5hbWU9ImZpbGwiIHZhbHVlcz0iI2VlZTsjZGRkOyNlZWUiIGR1cj0iMnMiIHJlcGVhdENvdW50PSJpbmRlZmluaXRlIiAvPgogICAgICA8L3JlY3Q+CiAgICA8L3N2Zz4="
+          tabIndex={-1}
+          className="tw-w-full tw-h-full tw-object-cover hover:tw-scale-105 tw-ease-in-out tw-transition-all tw-duration-200"
+          src={listing.images[0].url}
+        />
+      </div>
       <span className="tw-mt-2 sm:tw-mt-3 tw-font-bold tw-text-lg">{listing.name}</span>
       <span>{listing.location}</span>
       <span>
