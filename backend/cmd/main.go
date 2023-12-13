3cb048c33d69137196f8fbd82cee32e3bebe3689
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2023-12-13 16:37:22 -0500

    Remove one-off backfill job for descriptions

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	_ "image/gif"
@@ -13,13 +11,11 @@ import (
 	_ "image/png"
 
 	_ "golang.org/x/image/webp"
-	"gorm.io/gorm"
 
 	"go.fabra.io/server/common/application"
 	"go.fabra.io/server/common/auth"
 	"go.fabra.io/server/common/crypto"
 	"go.fabra.io/server/common/database"
-	"go.fabra.io/server/common/models"
 	"go.fabra.io/server/internal/api"
 	"go.fabra.io/server/internal/router"
 
@@ -46,37 +42,10 @@ func main() {
 		defer highlight.Stop()
 	}
 
-	updateDescriptions(db)
-
 	cryptoService := crypto.NewCryptoService()
 	authService := auth.NewAuthService(db, cryptoService)
 	apiService := api.NewApiService(db, authService, cryptoService)
 
 	router := router.NewRouter(authService)
 	router.RunService(apiService)
-}
-
-func updateDescriptions(db *gorm.DB) {
-	var listings []models.Listing
-	result := db.Table("listings").
-		Select("listings.*").
-		Where("listings.deactivated_at IS NULL").
-		Find(&listings)
-
-	if result.Error != nil {
-		log.Fatal(result.Error)
-		return
-	}
-
-	for _, listing := range listings {
-		if listing.Description != nil && !strings.Contains((*listing.Description), "</p>") {
-			newDescription := fmt.Sprintf("<p>%s</p>", strings.ReplaceAll((*listing.Description), "\n", "</p><p>"))
-			listing.Description = &newDescription
-			result := db.Save(&listing)
-			if result.Error != nil {
-				log.Fatal(result.Error)
-				return
-			}
-		}
-	}
 }
