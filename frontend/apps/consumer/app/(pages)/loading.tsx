04864f8e8753180c2604cbbe65308b120f574618
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-01-22 18:44:44 -0800

    Add min height to loading screen

--- frontend/apps/consumer/app/(pages)/loading.tsx
+++ frontend/apps/consumer/app/(pages)/loading.tsx
@@ -2,7 +2,7 @@ import { LogoLoading } from "@coaster/components/loading/LogoLoading";
 
 export default function Loading() {
   return (
-    <div className="tw-flex tw-justify-center tw-items-center tw-h-full">
+    <div className="tw-flex tw-justify-center tw-items-center tw-h-full tw-min-h-96">
       <LogoLoading className="tw-pb-48" />
     </div>
   );
