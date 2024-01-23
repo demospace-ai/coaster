ff18d40c3f3492dbf25a0045fa9a9097ac2fb64f
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-01-22 23:42:38 -0800

    Update place ID length

--- frontend/apps/consumer/app/api/commerce/listings/route.ts
+++ frontend/apps/consumer/app/api/commerce/listings/route.ts
@@ -1,30 +1,31 @@
-// import { SearchListings, sendRequest } from "@coaster/rpc/common";
-// import { NextRequest } from "next/server";
+import { SearchListings, sendRequest } from "@coaster/rpc/common";
+import { NextRequest } from "next/server";
 
-// const HEADER =
-//   "destination_id,name,type[0],url,address.addr1,address.city,address.country,latitude,longitude,neighborhood[0],image[0].url,image[0].tag[0],description,price,address.region";
+const HEADER =
+  "destination_id,name,type[0],url,address.addr1,address.city,address.country,latitude,longitude,neighborhood[0],image[0].url,image[0].tag[0],description,price,address.region";
 
-// export async function GET(_: NextRequest) {
-//   // TODO: authenticate request
-//   const listings = await sendRequest(SearchListings, { revalidate: 3600 });
-//   const data = await getAddressComponents(listings[0]?.place_id!);
-//   console.log(data);
-//   // const listingRows = listings.map((listing) => {
-//   //   const location = listing.location;
-//   //   return `${listing.id},${listing.name},${listing.categories?.[0]},https://www.trycoaster.com/listings/${listing.id},${listing.location},${listing.address.city},${listing.address.country},${listing.address.latitude},${listing.address.longitude},${listing.neighborhood[0]},${listing.images[0].url},Listing Image 1,${listing.description},${listing.price},${listing.address.region}`;
-//   // });
-//   return new Response("");
-// }
+export async function GET(_: NextRequest) {
+  // TODO: authenticate request
+  const listings = await sendRequest(SearchListings, { revalidate: 3600 });
+  const data = await getAddressComponents(listings[0]?.place_id!);
+  console.log(listings[0]);
+  console.log(data);
+  // const listingRows = listings.map((listing) => {
+  //   const location = listing.location;
+  //   return `${listing.id},${listing.name},${listing.categories?.[0]},https://www.trycoaster.com/listings/${listing.id},${listing.location},${listing.address.city},${listing.address.country},${listing.address.latitude},${listing.address.longitude},${listing.neighborhood[0]},${listing.images[0].url},Listing Image 1,${listing.description},${listing.price},${listing.address.region}`;
+  // });
+  return new Response("");
+}
 
-// async function getAddressComponents(placeId: string): Promise<string> {
-//   const url = new URL(`https://places.googleapis.com/v1/places/${placeId}`);
+async function getAddressComponents(placeId: string): Promise<string> {
+  const url = new URL("https://maps.googleapis.com/maps/api/place/details/json");
 
-//   const response = await fetch(url, {
-//     headers: {
-//       "X-Goog-FieldMask": "address_components",
-//       "X-Goog-Api-Key": process.env.NODE_MAPS_API_KEY ?? "",
-//     },
-//   });
-//   const results = await response.json();
-//   return results;
-// }
+  url.searchParams.append("place_id", placeId);
+  url.searchParams.append("fields", "address_components");
+  url.searchParams.append("key", process.env.NODE_MAPS_API_KEY ?? "");
+
+  console.log(url.toString());
+  const response = await fetch(url);
+  const results = await response.json();
+  return results;
+}
