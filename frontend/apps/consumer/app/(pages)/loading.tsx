fadfa764a2254be138788ba71661c777ffed06e9
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-01-22 18:42:21 -0800

    Updated loading logo image

--- frontend/apps/consumer/app/(pages)/loading.tsx
+++ frontend/apps/consumer/app/(pages)/loading.tsx
@@ -2,8 +2,8 @@ import { LogoLoading } from "@coaster/components/loading/LogoLoading";
 
 export default function Loading() {
   return (
-    <div className="tw-flex tw-justify-center tw-items-center tw-h-96">
-      <LogoLoading />
+    <div className="tw-flex tw-justify-center tw-items-center tw-h-full">
+      <LogoLoading className="tw-pb-48" />
     </div>
   );
 }
--- frontend/packages/components/src/loading/LogoLoading.tsx
+++ frontend/packages/components/src/loading/LogoLoading.tsx
@@ -1,23 +1,17 @@
-import Logo from "@coaster/assets/logo.svg";
+import LogoLoadingImage from "@coaster/assets/logo-loading.svg";
+import { mergeClasses } from "@coaster/utils/common";
 import Image from "next/image";
 
-export const LogoLoading: React.FC = () => {
+export const LogoLoading: React.FC<{ className: string }> = ({ className }) => {
   return (
     <Image
-      src={Logo}
+      src={LogoLoadingImage}
       width={144}
       height={144}
-      className="
-        tw-m-auto
-        tw-justify-center
-        tw-items-center
-        tw-rounded
-        tw-flex
-        tw-my-auto
-        tw-select-none
-        tw-animate-shimmer
-        [mask:linear-gradient(-60deg,#000_30%,#0005,#000_70%)_right/500%_100%]
-        "
+      className={mergeClasses(
+        className,
+        "tw-m-auto tw-justify-center tw-items-center tw-rounded tw-flex tw-my-auto tw-select-none tw-animate-shimmer [mask:linear-gradient(-60deg,#000_30%,#0005,#000_70%)_right/500%_100%]",
+      )}
       alt="coaster logo"
     />
   );
