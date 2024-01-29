52f2314edef73877415a259506adf2b3ab68b132
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-01-28 22:00:27 -0800

    Return response from root revalidation endpoint

--- frontend/apps/consumer/app/api/revalidate/route.ts
+++ frontend/apps/consumer/app/api/revalidate/route.ts
@@ -3,4 +3,6 @@ import { NextRequest } from "next/server";
 
 export async function POST(_: NextRequest) {
   revalidatePath("/(pages)", "page");
+
+  Response.json({ revalidated: true, now: Date.now() });
 }
